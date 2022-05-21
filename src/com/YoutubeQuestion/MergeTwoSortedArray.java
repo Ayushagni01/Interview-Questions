f5cd8e99de9f9a6e5a9fcb53d626d4f86023d75e
Ayushagni01 <ayush.ayush24@gmail.com>
2022-05-21 14:08:09 +0530

    Maths Functions

--- src/com/YoutubeQuestion/MergeTwoSortedArray.java
+++ src/com/YoutubeQuestion/MergeTwoSortedArray.java
@@ -1,5 +1,12 @@
 package com.YoutubeQuestion;
 
 public class MergeTwoSortedArray {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
---
+++ src/com/concept/MathClassConcpet.java
@@ -0,0 +1,41 @@
+package com.concept;
+
+public class MathClassConcpet {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		 int i1 = 27;
+		  int i2 = -45;
+		  double d1 = 84.6;
+		  double d2 = 0.45;
+		  double d3=34.4;
+		  
+		  //abs
+		  System.out.println(Math.abs(i2)); 
+		  //round
+		  System.out.println(Math.round(d1));
+		  
+		  System.out.println(Math.round(d3));
+		  
+		  System.out.println(Math.round(d2));
+		  
+		  //ciel
+		  System.out.println(Math.ceil(d1));
+		  
+		  System.out.println(Math.ceil(d3));
+		  
+		  System.out.println(Math.ceil(d2));
+		  
+		  //floor
+		  System.out.println(Math.floor(d1));
+		  
+		  System.out.println(Math.floor(d3));
+		  
+		  System.out.println(Math.floor(d2));
+		  
+		  
+	}
+}
