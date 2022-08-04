1d5bd5e60cdece58ed081d1980bc02d6ea601c75
Ayushagni01 <ayush.ayush24@gmail.com>
2022-08-04 19:15:58 +0530

    Flattening The HashMaps

--- src/com/LeetCode/Easy/RomanToInteger.java
+++ src/com/LeetCode/Easy/RomanToInteger.java
@@ -1,11 +1,40 @@
 package com.LeetCode.Easy;
 
+import java.util.*;
 public class RomanToInteger {
 
 	public static void main(String[] args) {
 
-		String s="";		
-
+		String s="MCMXCIV";		
+		System.out.print(RomanToInteger.romanToInt(s));
 	}
-
+	 public static int romanToInt(String s) {
+	        
+	        Map<Character,Integer> aa=new HashMap<>();
+	        aa.put('I',1);
+	        aa.put('V',5);
+	        aa.put('X',10);
+	        aa.put('L',50);
+	        aa.put('C',100);
+	        aa.put('D',500);
+	        aa.put('M',1000);
+	        int result=0;
+	        char c;
+	        for(int i=0;i<s.length();i++)
+	        {
+	            c=s.charAt(i);
+	            if(c=='I'){result=result+aa.get(c);}
+	            if(c=='V'){result=result+aa.get(c);}
+	            if(c=='X'){result=result+aa.get(c);}
+	            if(c=='L'){result=result+aa.get(c);}
+	            if(c=='C'){result=result+aa.get(c);}
+	            if(c=='D'){result=result+aa.get(c);}
+	            if(c=='M'){result=result+aa.get(c);}
+	            
+	            
+	        }
+	        return result;
+	        
+	        
+	    }
 }
